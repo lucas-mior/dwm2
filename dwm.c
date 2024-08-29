4bb2f58adb1fba236d11486ba32ec2a33f684fac
lucas-mior <lucas.mior@tutamail.com>
2024-08-29 12:35:09 -0300

    status_get_signal_number: use caching with dedicated struct (WIP)

--- dwm.c
+++ dwm.c
@@ -308,8 +308,8 @@ static int status_count_pixels(char *);
 static int update_geometry(void);
 static long get_window_state(Window);
 static void draw_bars(void);
-static void draw_status_text(char *, int, BlockSignal *, int); 
-static void status_get_signal_number(BlockSignal *, int, int);
+static void draw_status_text(char *, BlockSignal *, int); 
+static void status_get_signal_number(BlockSignal *, int);
 static void grab_keys(void);
 static void scan_windows_once(void);
 static void setup_once(void);
@@ -2441,8 +2441,9 @@ monitor_draw_bar(Monitor *monitor) {
     if (monitor == live_monitor) {
         drw_setscheme(drw, scheme[SchemeNormal]);
 
-        draw_status_text(top_status, top_status_pixels,
-                         top_blocks_signal, monitor->win_w);
+        draw_status_text(top_status, 
+                         top_blocks_signal,
+                         monitor->win_w - top_status_pixels);
         text_pixels = top_status_pixels;
     }
 
@@ -2546,8 +2547,9 @@ monitor_draw_bar(Monitor *monitor) {
     drw_setscheme(drw, scheme[SchemeNormal]);
     drw_rect(drw, 0, 0, (uint)monitor->win_w, bar_height, 1, 1);
     if (monitor == live_monitor) {
-        draw_status_text(bottom_status, bottom_status_pixels,
-                         bottom_blocks_signal, monitor->win_w);
+        draw_status_text(&bottom_status[2],
+                         bottom_blocks_signal,
+                         monitor->win_w - bottom_status_pixels);
     }
     drw_map(drw, monitor->bottom_bar_window,
             0, 0, (uint)monitor->win_w, bar_height);
@@ -3084,17 +3086,13 @@ handler_button_press(XEvent *event) {
             click = ClickBarLayoutSymbol;
         } else if (button_x > monitor->win_w - top_status_pixels) {
             click = ClickBarStatus;
-            status_get_signal_number(top_blocks_signal,
-                                     monitor->win_w - top_status_pixels,
-                                     button_x);
+            status_get_signal_number(top_blocks_signal, button_x);
         } else {
             click = ClickBarTitle;
         }
     } else if (button_event->window == monitor->bottom_bar_window) {
         click = ClickBottomBar;
-        status_get_signal_number(bottom_blocks_signal,
-                                 monitor->win_w - bottom_status_pixels,
-                                 button_x);
+        status_get_signal_number(bottom_blocks_signal, button_x);
     } else if ((client = window_to_client(button_event->window))) {
         client_focus(client);
         monitor_restack(monitor);
@@ -3119,28 +3117,29 @@ handler_button_press(XEvent *event) {
     return;
 }
 
-void draw_status_text(char *status, int status_pixels,
-                      BlockSignal *blocks, int mon_win_w) {
+void draw_status_text(char *status, BlockSignal *blocks, int x0) {
     char *text;
     int pixels = 0;
     int text_pixels = 0;
     int i = 0;
-    char byte;
+    char byte = *status;
+    status += 1;
 
     for (text = status; *status; status += 1) {
         if ((uchar)(*status) < ' ') {
+            blocks[i].signal = byte;
             byte = *status;
             *status = '\0';
 
             text_pixels = (int)(get_text_pixels(text) - text_padding);
 
-            blocks[i].min_x = mon_win_w - status_pixels + pixels;
+            blocks[i].min_x = x0 + pixels;
             blocks[i].max_x = blocks[i].min_x + text_pixels;
-            blocks[i].signal = byte;
+
             i += 1;
 
             drw_text(drw,
-                     mon_win_w - status_pixels + pixels, 0,
+                     x0 + pixels, 0,
                      (uint)text_pixels, bar_height, 0, text, 0);
             pixels += text_pixels;
 
@@ -3148,9 +3147,13 @@ void draw_status_text(char *status, int status_pixels,
             text = status + 1;
         }
     }
+    blocks[i].signal = byte;
     text_pixels = (int)(get_text_pixels(text) - text_padding + 2);
+    blocks[i].min_x = x0 + pixels;
+    blocks[i].max_x = blocks[i].min_x + text_pixels;
+    i += 1;
     drw_text(drw,
-             mon_win_w - status_pixels + pixels, 0,
+             x0 + pixels, 0,
              (uint)text_pixels, bar_height, 0, text, 0);
     return;
 }
@@ -3176,14 +3179,12 @@ status_count_pixels(char *status) {
 }
 
 void
-status_get_signal_number(BlockSignal *blocks, int x, int button_x) {
+status_get_signal_number(BlockSignal *blocks, int button_x) {
     status_signal = 0;
 
     for (int i = 0; i < STATUS_MAX_BLOCKS; i += 1) {
-        if (blocks[i].min_x > x && blocks[i].max_x < button_x)
+        if (blocks[i].min_x <= button_x && button_x <= blocks[i].max_x)
             status_signal = blocks[i].signal;
-        if (blocks[i].max_x >= button_x)
-            break;
     }
     return;
 }
