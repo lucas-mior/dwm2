8571c04b92af4b652f36f51b213a9921848c9580
lucas-mior <lucas.mior@tutamail.com>
2026-05-13 14:59:27 -0300

    ok

--- draw.h
+++ draw.h
@@ -3,6 +3,19 @@
 #if !defined(DRAW_H)
 #define DRAW_H
 
+#include <X11/cursorfont.h>
+#include <X11/keysym.h>
+#include <X11/Xatom.h>
+#include <X11/Xlib.h>
+#include <X11/Xproto.h>
+#include <X11/Xutil.h>
+
+#ifdef XINERAMA
+#include <X11/extensions/Xinerama.h>
+#endif /* XINERAMA */
+
+#include <X11/Xft/Xft.h>
+
 #include "util.c"
 
 typedef struct {
