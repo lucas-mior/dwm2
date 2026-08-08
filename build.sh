a5cd4357ca07cb5926d100a60080960ec493c608
lucas-mior <lucas.mior@tutamail.com>
2026-08-08 10:15:54 -0300

    update cbase

--- build.sh
+++ build.sh
@@ -47,7 +47,8 @@ FREETYPELIBS="-lfontconfig -lXft -lharfbuzz"
 
 CFLAGS="$CFLAGS -std=c11"
 CFLAGS="$CFLAGS -Wfatal-errors"
-CFLAGS="$CFLAGS -Werror"
+CFLAGS="$CFLAGS -Werror=all -Werror=extra"
+# CFLAGS="$CFLAGS -Werror"  # Only uncomment occasionally, keep this line
 CFLAGS="$CFLAGS -Wextra -Wall"
 CFLAGS="$CFLAGS -Wno-padded"
 CFLAGS="$CFLAGS -Wno-deprecated-declarations"
