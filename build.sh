bfa9eb79489a59ef04d7703ff5b9d0ae7f20d822
lucas-mior <lucas.mior@tutamail.com>
2026-08-12 16:03:31 -0300

    update cbase

--- build.sh
+++ build.sh
@@ -74,6 +74,7 @@ if [ "$CC" = "clang" ]; then
     CFLAGS="$CFLAGS -Wno-pre-c11-compat"
     CFLAGS="$CFLAGS -Wno-unsafe-buffer-usage"
     CFLAGS="$CFLAGS -Wno-unused-macros"
+    CFLAGS="$CFLAGS -Wno-used-but-marked-unused"
 fi
 
 exe="bin/$program"
