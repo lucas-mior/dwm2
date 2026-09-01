b05126fbd645b9f4d8ab645bbe9d0534eba449ff
lucas-mior <lucas.mior@tutamail.com>
2026-09-01 11:10:32 -0300

    update cbase

--- build.sh
+++ build.sh
@@ -58,7 +58,6 @@ CPPFLAGS="$CPPFLAGS $(pkg-config --cflags imlib2)"
 
 CFLAGS="$CFLAGS -std=c11"
 CFLAGS="$CFLAGS -Wfatal-errors"
-CFLAGS="$CFLAGS -Werror"  # Only uncomment occasionally, keep this line
 
 LDFLAGS="$LDFLAGS -lm"
 LDFLAGS="$LDFLAGS $(pkg-config --libs x11)"
