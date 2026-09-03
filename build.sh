02f83ebaa0ea41dae2fd6e684d3aa410c9c4c164
lucas-mior <lucas.mior@tutamail.com>
2026-09-03 00:43:46 -0300

    update cbase

--- build.sh
+++ build.sh
@@ -94,7 +94,7 @@ debug)
     ;;
 debug-fast)
     CFLAGS="$CFLAGS -g2 -O2 -flto -march=native -ftree-vectorize"
-    CFLAGS="$CFLAGS -fsanitize=undefined"
+    CFLAGS="$CFLAGS -fsanitize-trap=undefined"
     CPPFLAGS="$CPPFLAGS -DDEBUGGING=1"
     ;;
 build)
